333552e1ba41f0297d2bd1d4a4c88d30b3a4a704
Arthur Vardanyan <artie.vard@gmail.com>
2015-01-11 07:15:55 +0400

    do ex 3-8. there are still more ex.

--- homeworks/practice/p1.hs
+++ homeworks/practice/p1.hs
@@ -1,16 +1,58 @@
 {-# OPTIONS_GHC -Wall #-}
 
 module P1 where
+import Prelude hiding(gcd)
 default(Integer)
 
--- 1.
+-- 1. Positive Numbers
 isPositive :: (Num a, Ord a) => a  -> Bool
 isPositive  = (> 0)
 
--- 2.
-
+-- 2. Divisibility
+-- assume that the second argument will be non-zero.
 isDivisibleBy :: Int -> Int -> Bool
 -- isDivisibleBy n k = mod n k == 0
 isDivisibleBy n  = (==0) .  mod n
 
+-- 3. Integer Division
+-- assume that the 1st argument is non-negative
+-- and the second one is strictly positive.
+divideBy :: Int -> Int -> Int
+divideBy n d   | n < d = 0
+               |otherwise = 1 + divideBy (n-d) d
 
+-- 4. Greatest Common Divisor
+-- assume that both numbers are positive.
+gcd :: Int -> Int -> Int
+gcd n m |n==m = n
+        |n>m = gcd (n-m) m
+        |otherwise = gcd n (m-n)
+    
+
+-- 5. Least Common Multiple
+-- assume that both numbers are positive.
+
+lcm :: Int -> Int -> Int
+lcm n m = n * (m `div` gcd n m)
+
+
+
+-- 6. Greatest Common Divisor { Continued
+-- ssume that the list is non-empty and
+-- all the numbers on the list are positive.
+gcdList :: [Int] -> Int
+gcdList [x] = x
+gcdList (x:xs) = gcd x (gcdList xs)
+
+-- 7. Element Of A List
+anyDivisibleBy :: [Int] -> Int -> Bool
+anyDivisibleBy [] _ = False
+anyDivisibleBy (x:xs) d |isDivisibleBy d x = True
+                        |otherwise = anyDivisibleBy xs d
+--anyDivisibleBy xs d =  any (isDivisibleBy d) xs
+
+-- 8. Integer Division - Continued
+
+safeDivideBy :: Int -> Int -> Maybe Int
+safeDivideBy _ 0 = Nothing
+safeDivideBy n d = Just $ n `div` d
--- homeworks/practice/p1tests.hs
+++ homeworks/practice/p1tests.hs
@@ -3,6 +3,7 @@
 module P1tests where
 import Test.Hspec
 import P1
+import Prelude hiding(gcd, lcm)
 
 
 main :: IO ()
@@ -10,8 +11,29 @@ main = hspec $ do
   describe "Practice 1 problems tests" $ do
     it "Ex 1. isPositive" $ do
       isPositive (1::Int) `shouldBe` True
+      -- read Defaulting rules articles,
+      -- http://www.alexeyshmalko.com/2014/haskell-defaulting-rules/
     it "Ex 2. isDivisibleBy" $ do
       isDivisibleBy 6 3 `shouldBe` True
+    it "Ex 3. divideBy" $ do
+      divideBy 6 3 `shouldBe` 2
+      divideBy 7 3 `shouldBe` 2
+    it "Ex 4. Greatest Common Divisor" $ do
+      gcd 18 12 `shouldBe` 6
+      gcd 7 3 `shouldBe` 1
+    it "Ex 5. Least Common Multiple" $ do
+      lcm 18 12 `shouldBe` 36
+    it "Ex 6. GCD over list" $ do
+      gcdList [18, 12, 3] `shouldBe` 3
+    it "Ex 7. Element Of A List" $ do
+      anyDivisibleBy [13, 1, 20] 5 `shouldBe` True
+    it "Ex 8. Save DivideBy" $ do
+      safeDivideBy 7 3 `shouldBe` Just 2
+      safeDivideBy 7 0 `shouldBe` Nothing
+      
+          
+    
+      
     
 
 
