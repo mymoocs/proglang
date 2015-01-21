c315fedd45cc10f83ed6c7a63149b802f69ec710
Arthur Vardanyan <artie.vard@gmail.com>
2015-01-21 08:59:27 +0400

    olve 4-7 of p3.

--- homeworks/practice/p3.hs
+++ homeworks/practice/p3.hs
@@ -36,4 +36,43 @@ unfold f x = case f x of
 -- 3 A Novel Approach
 
 factorial :: Int -> Int
-factorial n = foldl (*) 1 $ unfold (\x -> if x > n then Nothing else Just (x+1, x)) 1
+factorial n = foldl (*) 1 $ unfold (\x -> if x > n
+                                          then Nothing
+                                          else Just (x+1, x)) 1
+
+-- 4. Unforeseen Developments
+
+unfoldMap :: (a -> b) -> [a] -> [b]
+unfoldMap f = unfold (\xs' -> case xs' of
+                               [] -> Nothing
+                               (x:xs) -> Just $ (xs, f x))
+                     
+-- 5. So Imperative (*)
+doUntil :: (a -> a) -> (a -> Bool) -> a -> a
+doUntil f p x = if p x then doUntil f p (f x) else x
+
+-- 6. Yet Another Factorial - Imperative
+
+impFactorial :: Integer -> Integer
+impFactorial n = fst $ doUntil (\(acc, r) -> (acc * r, r-1)) (\(_ , r) -> r > 0)
+                 (1, n)
+
+-- 7. Fixed Point (*)
+fixedPoint :: (Eq a) => (a -> a) -> a -> a
+fixedPoint f = doUntil f (\x -> f x /= x) 
+
+
+
+
+mySqrt :: Double -> Double
+mySqrt = mySqrt' 0.0001
+
+mySqrt' :: (Fractional a, Ord a) => a -> a -> a         
+mySqrt' eps n = doUntil f p n
+  where
+    f = (\x -> 0.5 * (x + n / x))
+    p = (\x -> abs(f x - x) > eps)
+
+    
+-- 8. Newton's Method
+-- square root is fixed point of 1/2 * (x+n/2)
--- homeworks/practice/p3tests.hs
+++ homeworks/practice/p3tests.hs
@@ -15,9 +15,21 @@ main = hspec $ do
       foldMap2 (*2) [1..3] `shouldBe` [2, 4, 6::Int]
       foldFilter  even [1..10] `shouldBe` [2, 4, 6::Int, 8, 10]
       foldFilter1 even [1..10] `shouldBe` [2, 4, 6::Int, 8, 10]      
-    it "unfold" $ do
+    it "Ex 2. unfold" $ do
       unfold (\x -> if x > 3 then Nothing else  Just (x+1, x)) 0
         `shouldBe` [0, 1, 2, 3::Int]
-    it "factorial using unfold and foldl" $ do
+    it "Ex 3. factorial using unfold and foldl" $ do
       factorial 4 `shouldBe` 24
 
+    it "Ex 4. map with unfold" $ do
+      unfoldMap (+1) [1..3] `shouldBe` [2,3,4::Int]
+
+    it "Ex 5. doUntil" $ do
+      doUntil (\x -> x+1) (<10) 1 `shouldBe` 10
+
+    it "Ex 6. Imperative factorial using doUntil" $ do
+      factorial 4 `shouldBe` 24
+
+    it "Ex. 7 sqrt by Newton's method(fixed point)" $ do
+      abs(mySqrt 2 - sqrt 2) < 0.01 `shouldBe` True
+      
